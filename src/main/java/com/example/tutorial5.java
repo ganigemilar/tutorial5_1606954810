d97f068a290c1129f253cafb5f749c61f9a7a5c3
ganigemilar <ganigemilar10@gmail.com>
2018-03-15 22:40:52 +0700

    tutorial 5 ver 1

---
+++ src/main/java/com/example/tutorial5.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tutorial5
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (tutorial5.class, args);
+    }
+}
